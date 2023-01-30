f5968f48b62373171fcbf959dceaea8a41819f0a
kuropenguin <kuro1994.t@gmail.com>
2023-01-30 21:19:34 +0900

    add get next peer

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ type Backend struct {
 }
 
 type ServerPool struct {
-	backend []*Backend
-	current uint64
+	backends []*Backend
+	current  uint64
 }
 
 func main() {
@@ -30,4 +30,19 @@ func main() {
 
 func (s *ServerPool) NextIndex() int {
 	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
+}
+
+func (s *ServerPool) GetNextPeer() *Backend {
+	next := s.NextIndex()
+	l := len(s.backends) + next
+	for i := next; i < l; i++ {
+		idx := i % len(s.backends)
+		if s.backends[idx].IsAlive() {
+			if i != next {
+				atomic.StoreUint64(&s.current, uint64(idx))
+			}
+			return s.backends[idx]
+		}
+	}
+	return nil
 }
